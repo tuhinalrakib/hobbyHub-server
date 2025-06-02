2166cd256b611ba015ba75d94e18e0be5cd657b8
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-06-03 01:23:00 +0600

    last modify users

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hobby Hub Server")
 })
 
@@ -18,93 +18,95 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    // User relted APIs
-    const userCollection = client.db("hobbyHubDb").collection("users")
-    const groupCollection = client.db("hobbyHubDb").collection("groups")
+        // User relted APIs
+        const userCollection = client.db("hobbyHubDb").collection("users")
+        const groupCollection = client.db("hobbyHubDb").collection("groups")
 
-    // users related APIS
-    app.get("/users",async(req,res)=>{
-        const email = req.query.email 
-        const query = { email : email}
-        const result = await userCollection.findOne(query)
-        res.send(result)
-    })
+        // users related APIS
+        app.get("/users", async (req, res) => {
+            const email = req.query.email
+            if (email) {
+                const query = { email: email }
+                const result = await userCollection.findOne(query)
+                res.send(result)
+            } else {
+                const result = await userCollection.find().toArray()
+                res.send(result)
+            }
 
-    app.post("/users", async(req,res)=>{
-        const newUser = req.body
-        const result = await userCollection.insertOne(newUser)
-        console.log(newUser)
-        res.send(result)
-    })
+        })
 
-    app.get("/users", async(req,res)=>{
-        const result = await userCollection.find().toArray()
-        res.send(result)
-    })
+        app.post("/users", async (req, res) => {
+            const newUser = req.body
+            const result = await userCollection.insertOne(newUser)
+            console.log(newUser)
+            res.send(result)
+        })
 
-    // Group Related APIS
-    app.post("/groups", async(req,res)=>{
-        const newGroup = req.body 
-        const result = await groupCollection.insertOne(newGroup)
-        res.send(result)
-    })
 
-    app.get("/groups", async(req,res)=>{
-        const result = await groupCollection.find().toArray()
-        res.send(result)
-    })
+        // Group Related APIS
+        app.post("/groups", async (req, res) => {
+            const newGroup = req.body
+            const result = await groupCollection.insertOne(newGroup)
+            res.send(result)
+        })
 
-    app.get("/groups/:id", async(req,res)=>{
-        const id = req.params.id
-        const filter = { _id : new ObjectId(id)}
-        const result = await groupCollection.findOne(filter)
-        res.send(result)
-    })
+        app.get("/groups", async (req, res) => {
+            const result = await groupCollection.find().toArray()
+            res.send(result)
+        })
 
-    app.delete("/groups/:id", async (req,res)=>{
-        const id = req.params.id 
-        const query = { _id : new ObjectId(id)}
-        const result = await groupCollection.deleteOne(query)
-        res.send(result)
-    })
+        app.get("/groups/:id", async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const result = await groupCollection.findOne(filter)
+            res.send(result)
+        })
 
-    app.put("/groups/:id", async (req,res)=>{
-        const id = req.params.id
-        const filter = { _id: new ObjectId(id) }
-        const options = { upsert: true }
-        const updatedCoffee = req.body
-        const updatedDoc = {
-            $set: updatedCoffee
-        }
-        const result = await groupCollection.updateOne(filter,updatedDoc,options)
-        res.send(result)
-    })
+        app.delete("/groups/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await groupCollection.deleteOne(query)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.put("/groups/:id", async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedCoffee = req.body
+            const updatedDoc = {
+                $set: updatedCoffee
+            }
+            const result = await groupCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App listening from: ${port}`)
 })
